b9cc0fd95a859c589d3073acadff89c47cf550c7
Alex <asigalov61@hotmail.com>
2020-11-26 04:43:45 -0800

    Bash script for RaspiAudio MIC+ board
    This will generate a composition, blink LED button, and will wait for a press to play the resulting MIDI.
    
    https://forum.raspiaudio.com/t/mic-installation-guide/17

---
+++ Raspberry-Pi-4-Edition/MM_RaspiAudio_MIC+.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#init led & button
+echo 25 >/sys/class/gpio/unexport
+echo 25 >/sys/class/gpio/export
+echo out >/sys/class/gpio/gpio25/direction
+echo 23 >/sys/class/gpio/export
+echo in >/sys/class/gpio/gpio23/direction
+#infinite loops
+while [ 1 ]
+do
+while [ 1 ]
+do
+#led ON
+cd ./Meddleying-MAESTRO/
+
+python3 MM_Generator.py&
+sleep 10
+echo 1 >/sys/class/gpio/gpio25/value
+
+echo  "------Please press the on Yellow button to listen to your composition"
+
+amixer set Micro 50%
+amixer set Master 96%
+sudo alsactl store
+
+#waiting button pressed
+while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
+set i = 1
+done
+#led OFF
+echo 0 >/sys/class/gpio/gpio25/value
+
+#led BLINK
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+
+
+echo "Playing the output MIDI file..."
+#play record
+timidity ./output.mid
+done
+echo "------------------------------------------------------------------------"
+done
+exit
+
+done
+echo 25 >/sys/class/gpio/unexport
+
+
