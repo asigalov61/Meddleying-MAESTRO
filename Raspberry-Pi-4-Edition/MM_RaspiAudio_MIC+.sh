e8b4b35f28e4536aa07cc323bf753fdd3dcde550
Alex <asigalov61@hotmail.com>
2020-11-29 22:49:37 -0800

    Raspi Audio MIC+ DAC HAT Version 2.0 Shell Script
    Added nice voice prompts and improved the script.

--- Raspberry-Pi-4-Edition/MM_RaspiAudio_MIC+.sh
+++ Raspberry-Pi-4-Edition/MM_RaspiAudio_MIC+.sh
@@ -5,11 +5,16 @@ echo 25 >/sys/class/gpio/export
 echo out >/sys/class/gpio/gpio25/direction
 echo 23 >/sys/class/gpio/export
 echo in >/sys/class/gpio/gpio23/direction
-#infinite loops
 
-while [ 1 ]
-do
+##Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
 
+##Welcome prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Hello_Listener_Welcome_Back.wav
+sleep 1
+
+echo "--------Main Outer Cycle-----------"
 while [ 1 ]
 do
 
@@ -21,6 +26,22 @@ sudo alsactl store
 #led ON
 echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
+
+##Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
+
+##Lets Make Music prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Lets_Make_Music.wav
+sleep 1
+
+##Play Back Last prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Play_Back_Last.wav
+sleep 1
+
+echo "---------Inner Main cycle------"
+while [ 1 ]
+do
 
 #waiting button pressed
 while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
@@ -41,6 +62,14 @@ sleep 1
 echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
 
+##Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
+
+##Generate Long Press prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Generate_Long_Press.wav
+sleep 1
+
 #waiting button pressed for 5 secons (delayed press)
 while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
 sleep 5
@@ -56,6 +85,10 @@ echo  "------Please press the on Yellow button to listen to your composition"
 echo 0 >/sys/class/gpio/gpio25/value
 sleep 1
 
+##Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
+
 #Remove last composition
 rm /home/pi/Meddleying-MAESTRO/output.mid
 sleep 1
@@ -64,6 +97,9 @@ echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
 echo 0 >/sys/class/gpio/gpio25/value
 sleep 1
+
+##Nice Make Music prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Nice_Make_Music.wav
 
 #led BLINK
 echo 1 >/sys/class/gpio/gpio25/value
@@ -87,6 +123,18 @@ sleep 1
 echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
 
+##Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
+
+##Composition Ready prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Composition_Ready.wav
+sleep 1
+
+##Nice Make Music prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Play_New.wav
+sleep 1
+
 #waiting button pressed
 #while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
 #set i = 1
@@ -96,6 +144,10 @@ sleep 1
 #sleep 1
 #echo 0 >/sys/class/gpio/gpio25/value
 #sleep 1
+
+#Chime prompt
+aplay /home/pi/Meddleying-MAESTRO/Voice_Prompts/Computer_Magic-Microsift-1901299923.wav
+sleep 1
 
 #echo "Playing the output MIDI file..."
 
