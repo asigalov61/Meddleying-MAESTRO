bf4365f4e74e3fbaeba68bd715d59080a058548d
Alex <asigalov61@hotmail.com>
2020-11-29 20:42:31 -0800

    Raspi Audio MIC+ DAC HAT Shell Script Version 2.0.
    Completely rewrote the script. Now it works as it should. Enjoy! :)

--- Raspberry-Pi-4-Edition/MM_RaspiAudio_MIC+.sh
+++ Raspberry-Pi-4-Edition/MM_RaspiAudio_MIC+.sh
@@ -6,26 +6,58 @@ echo out >/sys/class/gpio/gpio25/direction
 echo 23 >/sys/class/gpio/export
 echo in >/sys/class/gpio/gpio23/direction
 #infinite loops
+
 while [ 1 ]
 do
+
 while [ 1 ]
 do
-#led ON
 
-echo 1 >/sys/class/gpio/gpio25/value
-
-echo  "------Please press the on Yellow button to listen to your composition"
-
+#Set volume
 amixer set Micro 50%
 amixer set Master 99%
 sudo alsactl store
+
+#led ON
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
 
 #waiting button pressed
 while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
 set i = 1
 done
+echo  "------Please press the on Yellow button to listen to your composition"
+
+#led ON
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+
+#play last record
+timidity /home/pi/Meddleying-MAESTRO/output.mid
+sleep 1
+
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+
+#Remove last composition
+rm /home/pi/Meddleying-MAESTRO/output.mid
+sleep 1
+
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+
+#waiting button pressed
+while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
+sleep 5
+set i = 1
+done
+echo  "------Please press the on Yellow button to listen to your composition"
+
 #led OFF
 echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
 
 #led BLINK
 echo 1 >/sys/class/gpio/gpio25/value
@@ -36,20 +68,40 @@ echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
 echo 0 >/sys/class/gpio/gpio25/value
 sleep 1
+
+#Generate
+cd /home/pi/Meddleying-MAESTRO/
+python3 /home/pi/Meddleying-MAESTRO/MM_Generator.py
+sleep 5
+
 echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
 echo 0 >/sys/class/gpio/gpio25/value
-
-
-echo "Playing the output MIDI file..."
-#play record
-timidity /home/pi/Meddleying-MAESTRO/output.mid
-sleep 1
-cd ./Meddleying-MAESTRO/
-python3 /home/pi/Meddleying-MAESTRO/MM_Generator.py
 sleep 1
 echo 1 >/sys/class/gpio/gpio25/value
 sleep 1
+
+#waiting button pressed
+while [ `cat /sys/class/gpio/gpio23/value` = 1 ]; do
+set i = 1
+done
+
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+
+echo "Playing the output MIDI file..."
+
+play record
+timidity /home/pi/Meddleying-MAESTRO/output.mid
+sleep 1
+
+echo 1 >/sys/class/gpio/gpio25/value
+sleep 1
+echo 0 >/sys/class/gpio/gpio25/value
+sleep 1
+
 done
 echo "------------------------------------------------------------------------"
 done
@@ -57,5 +109,3 @@ exit
 
 done
 echo 25 >/sys/class/gpio/unexport
-
-
