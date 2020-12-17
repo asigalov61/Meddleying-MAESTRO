1e10a5c3636a41276d759675ec25099c882627bb
Alex <asigalov61@hotmail.com>
2020-12-17 05:56:38 -0800

    Version 2.0. Now with an integrated MIDI player :)
    With SF2 support and nice sound. Enjoy! :)

--- Simple_MIDI_Flask_App/app.py
+++ Simple_MIDI_Flask_App/app.py
@@ -14,20 +14,18 @@ def file_downloads():
             <link href="styles/style.css" rel="stylesheet" type="text/css">
           </head>
           <body>
-            <center><p><br><br><br><br><br><br><br><br><br><br><br><br><br><br></p></center>
+            <center><p><br><br><br><br><br></p></center>
             <center><h1 style="color:Black;">Meddleying-MAESTRO Music</h1></center>
-
+            <section id="section3">
             <center>
-            <style>
-            body {
-              background-image: url('https://www.pythonanywhere.com/user/asigalov61/files/home/asigalov61/site/static/logo.jpeg');
-              background-repeat: no-repeat;
-              background-attachment: bottom;
-              background-size: cover;
-            }
-            </style>
+            <midi-player
+              src="https://asigalov61.pythonanywhere.com/static/output.mid"
+              sound-font visualizer="#section3 midi-visualizer">
+            </midi-player>
             </center>
-            <center><a href="/file"><button>Download new sample MIDI file every 20 minutes</button></a></center>
+            </section>
+            <center><a href="/file"><button>Download new sample MIDI file every 30 minutes</button></a></center>
+            <script src="https://cdn.jsdelivr.net/combine/npm/tone@14.7.58,npm/@magenta/music@1.21.0/es6/core.js,npm/focus-visible@5,npm/html-midi-player@1.1.0"></script>
             <center><h1 style="color:Black;">Project Los Angeles</h1></center>
             <center><h1 style="color:Black;">Tegridy Code 2020</h1></center>
             <br><center><a href="https://github.com/asigalov61/Meddleying-MAESTRO">Meddleying MAESTRO GitHub Repo</a><br>
@@ -39,7 +37,7 @@ def file_downloads():
 @app.route('/file')
 def return_files():
     return send_file(
-        'output.mid',
+        'static/output.mid',
         mimetype='audio/mid',
         attachment_filename='Meddleying_Maestro_Composition.mid',
         as_attachment=True
